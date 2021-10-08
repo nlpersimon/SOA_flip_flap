f05937d06a98cc94f644551b8b4b066b399f76e5
Simon Lee <simon2020291@gmail.com>
2021-10-09 07:31:11 +0800

    "All tests pass; rubocop/flog resolved"

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,28 @@
+# frozen_string_literal: true
+
+require 'csv'
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  def initialize
+    @tab = "\t"
+  end
+
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    @data = CSV.parse(tsv, col_sep: @tab, headers: true).map(&:to_h)
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    headers = @data.first.keys
+    CSV.generate(col_sep: @tab) do |csv|
+      csv << headers
+      @data.each do |row|
+        csv << headers.map { |field| row[field] }
+      end
+    end
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
+
+require 'yaml'
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yaml - a String in YAML format
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  # to_tsv: converts @data into yaml string
+  # returns: String in YAML format
+  def to_yaml
+    @data.to_yaml
+  end
+end
